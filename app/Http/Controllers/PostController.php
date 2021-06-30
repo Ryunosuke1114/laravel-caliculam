b2ad3d742cb5dad70c5c3bece0a1165f0710e516
Ryunosuke1114 <Ryunosuke1114@gmail.com>
2021-06-30 17:04:55 +0900

    create and validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-
         return view ('index')->with(['posts'=> $post->getPaginateByLimit(1)]);
 
     }
@@ -18,5 +17,18 @@ class PostController extends Controller
     {
         return view ('show')->with(['post' => $post ]);
     }
+    
+    public function create()
+    {
+        
+        return view ('create');
+    }
+    
+    public function store (Post $post , PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+        
+    }
 }
-
